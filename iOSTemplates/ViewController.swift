a74a7958c91ac5176ae6ac018a034dbd1ac079aa
Mario Galvão <mcasarigalvao@gmail.com>
2020-04-18 11:31:13 -0300

    Initial Commit

---
+++ iOSTemplates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTemplates
+//
+//  Created by Mário Galvao on 18/04/20.
+//  Copyright © 2020 Mário Galvao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
